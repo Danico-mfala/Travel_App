55b2daa8182aa2dfb561bd4bc5b269415f4527bf
Danico-Mfala <mfaladan@hotmail.com>
2023-05-05 19:47:38 +0300

    working on the navBar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./navbar.css";
+import { MdOutlineTravelExplore } from "react-icons/md";
 
 const Navbar = () => {
   return (
@@ -7,8 +8,51 @@ const Navbar = () => {
       <header className="header flex">
         <div className="logoDiv">
           <a href="#" className="logo">
-            <h1>Travel.</h1>
+            <h1>
+              {" "}
+              <MdOutlineTravelExplore className="icon" />
+              Travel.
+            </h1>
           </a>
+        </div>
+        <div className="navBar">
+          <ul className="navLists grid">
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Home
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Packages
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Shop
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">
+                About
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Pages
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">
+                News
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Contact
+              </a>
+            </li>
+          </ul>
         </div>
       </header>
     </section>
